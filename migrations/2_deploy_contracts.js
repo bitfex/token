648a8047a5be8f83b5207d27ae788d37931c339e
BitFex <support@bitfex.trade>
2018-07-31 21:59:26 +0300

    Update ICO stop date

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -16,7 +16,7 @@ module.exports = function (deployer, network, accounts) {
   const ICO = {
     openingTime: web3.eth.getBlock('latest').timestamp + 2, // two secs in the future
     // openingTime: 1535760000 // Sat, 01 Sep 2018 00:00:00 UTC +00:00
-    closingTime: 1537487999, // Thu, 20 Sep 2018 23:59:59 UTC +00:00
+    closingTime: 1541030399, // Wed, 31 Oct 2018 23:59:59 UTC +00:00
     price: new web3.BigNumber(web3.toWei(0.65, 'ether')), // ICO rate (1 wei is 10 token-bits)
     goal: new web3.BigNumber(web3.toWei(1000, 'ether')), // ICO soft cap
     hardCap: 3000000 // 30000.00 BITFEX Tokens
